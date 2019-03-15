a98aa26057691ef5a12dfd4e62ae29af1583cc61
Albert <albertcabrerizo@gmail.com>
2019-03-15 20:10:10 +0100

    Part1 Step4 Imports and Show random words

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
         appBar: new AppBar(
           title: const Text('MyApp'),
         ),
-        body: const Center(
-          child: const Text('Hi Albert this is Flutter'),
+        //Center has to be new because Text is new
+        body: new Center(
+          child: new Text(wordPair.asPascalCase), //every word start by uppercase
         ),
       ),
     );
